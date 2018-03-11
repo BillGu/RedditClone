4bd2a30027c713a51dc7e9d8c6a7fec34651b36f
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-03-11 17:49:10 +0800

    Added APIs to upvote/downvote a topic + rewrote API to add a topic

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 //simple in-memory data structure
 var data = [{"Id": 1, "Topic": "Rahul", "Votes": "1"}];
+var size = 1;
 
 //main message (to check if API Server is up!)
 app.get('/',function (req,res) {
@@ -26,9 +27,50 @@ app.get('/',function (req,res) {
 //POST a new topic
 //TODO: check if topic already exists and return error message
 app.post('/topic', bodyParser.urlencoded(), function(req, res) {
+	
+	var lastId = data[size - 1]["Id"];
+
 	var topic = req.body.topic;
-	data[topic] = 0;
+	var row = {"Id": lastId + 1, "Topic": topic, "Votes": 0};
+
+	data.push(row);
+	size++;
+
 	res.send(JSON.stringify({text : "Succesfully Added Topic!"}));
+});
+
+//Upvote a topic
+app.get('/topic/upvote/:id', bodyParser.urlencoded(), function(req, res) {
+
+	var id = req.params.id;
+
+	if (id >= 0 && id < size && data[id] != null) {
+
+		var row = data[id];
+		row["Votes"]++;
+		res.send(JSON.stringify({text : "Succesfully Upvoted on " + row["Topic"]}));
+
+	} else {
+		res.status(401).send("Error in id value passed in");
+	}
+
+});
+
+//Downvote a topic
+app.get('/topic/downvote/:id', bodyParser.urlencoded(), function(req, res) {
+
+	var id = req.param.id;
+
+	if (Number.isInteger(id) && id > 0 && id < size && data[id] != null) {
+
+		var row = data[id];
+		row["Votes"]--;
+		res.send(JSON.stringify({text : "Succesfully Downvoted on " + data["Topic"]}));
+
+	} else {
+		res.status(401).send("Error in id value passed in");
+	}
+
 });
 
 //GET all topics
